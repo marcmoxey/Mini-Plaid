601d6b8f0bfb884eb342a1b2be35239b1a5e3dda
marcmoxey <moxeymarc@gmail.com>
2026-04-10 09:29:03 -0400

    Add routes and controller for users

--- Routes/users.js
+++ Routes/users.js
@@ -1,7 +1,23 @@
-import express from 'express';
-import {} from '../Controllers/userController.js';
+import express from "express";
+import { createUser, getUser, getUsers, updateUser, deleteUser } from "../Controllers/userController.js";
 
 const router = express.Router();
 
 // Routes
- 
+
+// Get All users
+router.get("/", getUsers);
+
+// get single user 
+router.get("/:id", getUser);
+
+// Create new user
+router.post("/", createUser);
+
+// Update user 
+router.put("/:id", updateUser);
+
+// Delete user
+router.delete("/:id", deleteUser);
+
+export default router;
--- server.js
+++ server.js
@@ -1,28 +1,27 @@
-import express from "express"; 
+import express from "express";
 import path from "path";
-import url from "url"; 
-import users from './Routes/users.js';
+import url from "url";
+import users from "./Routes/users.js";
 import logger from "./Middleware/logger.js";
 import errorHandler from "./Middleware/error.js";
 import notFound from "./Middleware/notFound.js";
 
 const port = process.env.port || 8000;
 
-const __filename = url.fileURLToPath(import.meta.url); 
+const __filename = url.fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
 
 const app = express();
 
-// Body parser middleware 
-app.use(express.json()); 
-app.use(express.urlencoded({ extended: false}));
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-// Logger middleware 
-app.use(logger)
+// Logger middleware
+app.use(logger);
 
-// Routes 
-app.use('/api/users', users);
-
+// Routes
+app.use("/api/users", users);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
